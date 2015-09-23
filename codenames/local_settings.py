dcc69927cdc09442688046a483cb84a1ed5751d8
Danielpearl <danielaaronpearl@gmail.com>
2015-09-23 13:17:32 -0700

    setup framework for django

---
+++ codenames/local_settings.py
@@ -0,0 +1,12 @@
+__author__ = 'DanielPearl'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'codenames',
+        'USER': 'dpearl',
+        'PASSWORD': 'codenames',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
--- codenames/settings.py
+++ codenames/settings.py
@@ -100,3 +100,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Local Settings
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
