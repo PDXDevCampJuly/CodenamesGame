626e3b607856d345960850131e5d51c054962f5e
Danielpearl <danielaaronpearl@gmail.com>
2015-09-23 16:10:30 -0700

    null values removed because they are standard

--- spygame/models.py
+++ spygame/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 class Player(models.Model):
     team_id = models.ForeignKey(Team)
-    name = models.CharField(max_length=30, blank=True, null=False)
-    type = models.CharField(max_length=30, blank=True, null=False)
+    name = models.CharField(max_length=30, blank=False)
+    type = models.CharField(max_length=30, blank=False)
 
     def __str__(self):
         return self.name
 
 class Team(models.Model):
-    color = models.CharField(max_length=30, blank=True, null=False)
+    color = models.CharField(max_length=30, blank=False)
 
     def __str__(self):
         return self.color
@@ -18,23 +18,23 @@ class Team(models.Model):
 class Card(models.Model):
     game_id = models.ForeignKey(Game)
     dict_word = models.ForeignKey(Dictionary)
-    color = models.CharField(max_length=30, blank=True, null=False)
-    x_coord = models.IntegerField(max_length=25, blank=True, null=False)
-    y_coord = models.IntegerField(max_length=25, blank=True, null=False)
+    color = models.CharField(max_length=30, blank=False)
+    x_coord = models.IntegerField(max_length=25, blank=False)
+    y_coord = models.IntegerField(max_length=25, blank=False)
     revealed = models.BooleanField()
 
     def __str__(self):
         return self.word
 
 class Dictionary(models.Model):
-    word = models.CharField(max_length=30, blank=True, null=False)
+    word = models.CharField(max_length=30, blank=False)
 
     def __str__(self):
         return self.word
 
 class Clue(models.Model):
-    word = models.CharField(max_length=30, blank=True, null=False)
-    number = models.IntegerField(max_length=25, blank=True, null=False)
+    word = models.CharField(max_length=30, blank=False)
+    number = models.IntegerField(max_length=25, blank=False)
     game_id = models.ForeignKey(Game)
     team_id = models.ForeignKey(Team)
 
@@ -45,6 +45,6 @@ class Game(models.Model):
     turn_status = models.CharField()
     blueteam_id = models.ForeignKey(Team)
     redteam_id = models.ForeignKey(Team)
-    winner = models.CharField(max_length=30, blank=True, null=False)
-    password = models.IntegerField(max_length=30, blank=True, null=False)
+    winner = models.CharField(max_length=30, blank=False)
+    password = models.IntegerField(max_length=30, blank=False)
 
