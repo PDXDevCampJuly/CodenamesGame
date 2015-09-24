4acdd10a923a4a94e03800196e29f5ef6b8f53cf
Danielpearl <danielaaronpearl@gmail.com>
2015-09-24 16:05:16 -0700

    urls added

--- spygame/urls.py
+++ spygame/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
+from spygame import views
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'codenames.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$/', 'spygame.views', namespace="spygame"),
-
+    url(r'^$/', 'views.get_spy_page', name="get_spy_page"),
+    url(r'^$/', 'views.get_spymaster_page', name="get_spymaster_page"),
+    url(r'^$/', 'views.join_or_create', name="join_or_create"),
+    url(r'^$/', 'views..spy_or_spymaster', name="spy_or_spymaster"),
 ]
--- spygame/views.py
+++ spygame/views.py
@@ -10,3 +10,81 @@ def Player(request):
     return HttpResponse(json.dumps(data), content_type='application/json')
 # Create your views here.
 #data = serializers.serialize("json", Player.objects.all())
+
+def get_spy_page(request):
+    return render(request, 'spygamepg.html')
+
+def get_spymaster_page(request):
+    return render(request, 'spymastergamepg.html')
+
+def join_or_create(request):
+    return render(request, 'Homepg.html')
+
+def spy_or_spymaster(request):
+    return render(request, 'pickSpyOrSpymaster.html')
+
+def assign_players(): # assign player to team
+    pass
+
+def assign_spymaster(): # assign spymaster to team
+    pass
+
+def get_words(): # assign word to card
+    pass
+
+def get_clue(): # allows spymaster to enter clue
+    pass
+
+def validate_clue(): # can clue be entered by spymaster
+    pass
+
+def update_cards(): # updates card with revealed attributes
+    pass
+
+def set_count(): #sets count from clue_num set by spymaster
+    pass
+
+def change_count(): # changes count of clicks players can make per turn
+    pass
+
+def validate_click(): # validates that a player click is valid
+    pass
+
+def switch_turn(): # returns # of team
+    """
+    :return:
+    """
+    pass
+
+def validate_win(): #validates if player has won
+    pass
+
+def pass_move(): #allows player to pass turn
+    pass
+
+def remove_cards(): #replaces word on card with image
+    pass
+
+def card_click(): #allows user to click card
+    pass
+
+def prevent_click(): #prevents user from clicking board
+    pass
+
+def is_opponent(): #determines if card selected is opponent's card
+    pass
+
+def is_bystander(): #determines if card selected is bystander
+    pass
+
+def is_ally(): #determines if card selected is ally
+    pass
+
+def is_assassin(): #determines if card selected is assassin
+    pass
+
+def validate_clue_num(): #determines if spymaster clue num is valid
+    pass
+
+def main():
+    pass
