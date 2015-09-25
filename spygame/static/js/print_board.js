9defc2f4d5b0f860b4c5e041fe3f23d3a8a6f77b
Danielpearl <danielaaronpearl@gmail.com>
2015-09-25 11:18:14 -0700

    print board fixed

--- spygame/static/js/print_board.js
+++ spygame/static/js/print_board.js
@@ -1,6 +1,6 @@
 
 function create_board(){
-    var list = ["0","1","2","3","4","5","6","7", "8", "9", "10", "11", ];
+    var list = ["0","1","2","3","4","5","6","7", "8", "9", "10", "11", "12","13","14","15","16","17", "18", "19", "20", "21","22","23", "24","25"];
 
     var table = document.getElementById('boardwrapper'); //Appends table to html
 
@@ -8,11 +8,12 @@ function create_board(){
 
     for (var i = 0; i < 5; i++){
         var new_tr = document.createElement('tr'); //creates new tr
+
         for(var j = 0; j <= 4; j++){
 
             var new_td = document.createElement('td'); //creates new td
 
-            new_td.textContent=list[j];
+            new_td.textContent=list[5*i +j]; //Saves the correct card indexes
 
             new_tr.appendChild(new_td); //appends td to tr
         }
