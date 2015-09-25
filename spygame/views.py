698a40d4c62abdafd6bc96ee2d5c364432b9fa2f
Danielpearl <danielaaronpearl@gmail.com>
2015-09-25 14:06:53 -0700

    homepage modified to forgo code

--- spygame/views.py
+++ spygame/views.py
@@ -4,8 +4,6 @@ from django.core import serializers
 from .models import *
 import json
 from django.http import HttpResponse
-from djnago.view.generic import DetailView
-from books.models import Publisher
 
 def Player(request):
     data = []
