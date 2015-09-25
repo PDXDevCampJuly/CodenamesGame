f114c964d08aa8d68f70def82d84080323ed554e
Danielpearl <danielaaronpearl@gmail.com>
2015-09-25 10:56:09 -0700

    starting to revise print board

--- spygame/static/js/print_board.js
+++ spygame/static/js/print_board.js
@@ -1,6 +1,6 @@
 
 function create_board(){
-    var list = ["0","1","2","3","4","5","6"];
+    var list = ["0","1","2","3","4","5","6","7", "8", "9", "10", "11", ];
 
     var table = document.getElementById('boardwrapper'); //Appends table to html
 
